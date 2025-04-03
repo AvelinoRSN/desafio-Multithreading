e55bcd8bf451f606ddace807108bd974c1dd2d1e
Avelino Ramos <avelinorsilvaneto@gmail.com>
2025-04-02 21:41:49 -0300

    feat: add request ViaCep and BrasilAPI

--- main.go
+++ main.go
@@ -8,13 +8,29 @@ import (
 	"time"
 )
 
+type BrasilAPIResponse struct {
+	CEP        		string `json:"cep"`
+	Street 	   		string `json:"street"`	
+	Neighborhood    string `json:"neighborhood"`
+	City     		string `json:"city"`	
+	State     		string `json:"state"`	
+}
+
+type ViaCEPResponse struct {
+	CEP        		string `json:"cep"`
+	Logradouro 		string `json:"logradouro"`
+	Bairro     		string `json:"bairro"`
+	Cidade     		string `json:"localidade"`
+	Estado     		string `json:"uf"`
+}
+
 type Endereco struct {
-	CEP        string `json:"cep"`
-	Logradouro string `json:"logradouro"`	
-	Bairro     string `json:"bairro"`
-	Cidade     string `json:"localidade"`	
-	Estado     string `json:"uf"`	
-	Origem     string
+	CEP        		string
+	Logradouro 		string
+	Bairro     		string
+	Cidade     		string
+	Estado     		string
+	Origem     		string
 }
 
 func main() {
@@ -25,8 +41,8 @@ func main() {
 
 	ch := make(chan Endereco,2)
 
-	go BuscarViaCEP(ctx, cep, ch)
 	go BuscarBrasilAPI(ctx, cep, ch)
+	go BuscarViaCEP(ctx, cep, ch)
 
 	select {
 	case endereco := <-ch:
@@ -39,7 +55,7 @@ func main() {
 }
 
 func BuscarBrasilAPI(ctx context.Context, cep string, ch chan<- Endereco) {
-	url := "https://brasilapi.com.br/api/cep/v1/"
+	url := "https://brasilapi.com.br/api/cep/v1/"+ cep 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {	
 		return
@@ -49,14 +65,22 @@ func BuscarBrasilAPI(ctx context.Context, cep string, ch chan<- Endereco) {
 	if err != nil {
 		return
 	}
+	defer res.Body.Close()
 
-	var endereco Endereco
-	err = json.NewDecoder(res.Body).Decode(&endereco)
+	var apiResp BrasilAPIResponse
+	err = json.NewDecoder(res.Body).Decode(&apiResp)
 	if err != nil {
 		return
 	}
-	endereco.Origem = "BrasilAPI"
-	ch <- endereco
+	end := Endereco{
+		CEP:        apiResp.CEP,
+		Logradouro: apiResp.Street,
+		Bairro:     apiResp.Neighborhood,
+		Cidade:     apiResp.City,
+		Estado:     apiResp.State,	
+		Origem:     "BrasilAPI",
+	}
+	ch <- end
 }
 
 func BuscarViaCEP(ctx context.Context, cep string, ch chan<- Endereco){
@@ -71,11 +95,18 @@ func BuscarViaCEP(ctx context.Context, cep string, ch chan<- Endereco){
 	}
 	defer res.Body.Close()
 
-	var endereco Endereco
-	err = json.NewDecoder(res.Body).Decode(&endereco)
+	var apiResp ViaCEPResponse
+	err = json.NewDecoder(res.Body).Decode(&apiResp)
 	if err != nil {
 		return
 	}
-	endereco.Origem = "ViaCEP"
-	ch <- endereco
+	end := Endereco{
+		CEP:        apiResp.CEP,
+		Logradouro: apiResp.Logradouro,
+		Bairro:     apiResp.Bairro,
+		Cidade:     apiResp.Cidade,
+		Estado:     apiResp.Estado,	
+		Origem:     "ViaCEP",
+	}
+	ch <- end
 }
